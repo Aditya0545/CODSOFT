d41b8726ff209f94c7f8dfc1fcb91726e4227619
Aditya Kumar <105490754+Aditya0545@users.noreply.github.com>
2023-11-24 17:33:20 +0530

    updateed

--- Level-2/script.js
+++ Level-2/script.js
@@ -7,18 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-document.addEventListener("DOMContentLoaded", function() {
-    // Get all elements with class "content"
-    var skillItems = document.querySelectorAll(".content");
-
-    // Add event listeners for mouseover and mouseout
-    skillItems.forEach(function(item) {
-        item.addEventListener("mouseover", function() {
-            this.style.backgroundColor = "rgba(255, 255, 255, 0.2)"; // Change to your desired hover color
-        });
-
-        item.addEventListener("mouseout", function() {
-            this.style.backgroundColor = "transparent"; // Change to the default background color
-        });
-    });
-});
